421232e846a5dd2fc522bec272b06cda40d4fa3d
Gabriel Dzharadat <dzh.gabriel@gmail.com>
2024-03-25 14:34:59 +0000

    added API data call

---
+++ src/containers/BucketListContainer.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import Country from "../components/Country";
+import CountryForm from "../components/CountryForm";
+import CountriesToVisit from "../components/CountriesToVisit";
+import VisitedCountries from "../components/VisitedCountries";
+
+const BucketListContainer = () => {
+    
+    const [countries, setCountries] = useState([]);
+    const [countriesToVisit, setCountriesToVisit] = useState([]);
+    const [visitedCountries, setVisitedCountries] = useState([]);
+
+    const fetchCountries = async () => {
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        const data = await response.json();
+        setCountries(data);
+    }
+
+    // Get API data when page loads
+    useEffect(() => {
+        fetchCountries();
+    } , []);
+
+    return (
+        <>
+            <CountryForm />
+            <CountriesToVisit />
+            <VisitedCountries />
+        </>
+    )
+}
+
+export default BucketListContainer;
